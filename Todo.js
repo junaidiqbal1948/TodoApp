cd96669f54e97d3e3f1699118b4a67203cd9aea8
junaidiqbal1948 <junaidiqbal1948@gmail.com>
2018-11-25 21:31:04 +0500

    Refactored js code working fine

--- Todo.js
+++ Todo.js
@@ -8,57 +8,59 @@ var todo_wrap = document.getElementById("todo_wrap");
 var todo_text = document.getElementsByClassName("todo_text");
 // div completed
 var completed = document.getElementById('completed');
+// temporary len variable
+var tempLen;
 
-// initial for loop to add event listeners to span elements of todos
-for (var i = todo_text.length - 1; i >= 0; i--) {
-	todo_text[i].addEventListener("click", function(e){if (e.currentTarget.style.textDecoration === "line-through") {
-		e.currentTarget.style.textDecoration = "none";
-		todo_wrap.appendChild(e.currentTarget.parentNode);
-	}else{
-		e.currentTarget.style.textDecoration = "line-through";
-		completed.appendChild(e.currentTarget.parentNode);
-	}})
-}
 
 function updateEventTodo() {
-	if (document.querySelectorAll("#completed .todo").length>0)
-	{
-		todo_text[todo_text.length-(document.querySelectorAll("#completed .todo").length+1)].addEventListener("click", function(e){if (e.currentTarget.style.textDecoration === "line-through") {
-			e.currentTarget.style.textDecoration = "none";
-			todo_wrap.appendChild(e.currentTarget.parentNode);
-			console.log("hey");
-		}else{
-			e.currentTarget.style.textDecoration = "line-through";
-			completed.appendChild(e.currentTarget.parentNode);
-		}console.log(todo_text)})
-	} else
-	{
-		todo_text[todo_text.length-1].addEventListener("click", function(e){if (e.currentTarget.style.textDecoration === "line-through") {
-			e.currentTarget.style.textDecoration = "none";
-			todo_wrap.appendChild(e.currentTarget.parentNode);
-			console.log("hey");
-		}else{
-			e.currentTarget.style.textDecoration = "line-through";
-			completed.appendChild(e.currentTarget.parentNode);
-		}console.log(todo_text)})
-	}
-}
-
-// initial for loop to add event listeners to icon elements of todos
-for (var i = icon.length - 1; i >= 0; i--) {
-	 icon[i].addEventListener("click", function(e){e.currentTarget.parentNode.remove();})
-}
-
-function updateEvent() {
 	if (document.querySelectorAll("#completed .todo").length>0) {
-		icon[todo_text.length-(document.querySelectorAll("#completed .todo").length+1)].addEventListener("click", function(e){e.currentTarget.parentNode.remove();})
+		tempLen = todo_text.length-(document.querySelectorAll("#completed .todo").length+1);
 	} else {
-		icon[icon.length-1].addEventListener("click", function(e){e.currentTarget.parentNode.remove();})
+		tempLen = todo_text.length-1;
 	}
-
+	todo_text[tempLen].addEventListener("click", function(e) {
+		if (e.currentTarget.style.textDecoration === "line-through") {
+			e.currentTarget.style.textDecoration = "none";
+			todo_wrap.appendChild(e.currentTarget.parentNode);
+		} else {
+			e.currentTarget.style.textDecoration = "line-through";
+			completed.appendChild(e.currentTarget.parentNode);
+		}
+	});
 }
 
+function updateEventIcon() {
+	if (document.querySelectorAll("#completed .todo").length>0) {
+		// some todo completed
+		tempLen = todo_text.length-(document.querySelectorAll("#completed .todo").length+1);
+	} else {
+		// todo not completed any of yet
+		tempLen = icon.length-1;
+	}
+	icon[tempLen].addEventListener("click", function(e){e.currentTarget.parentNode.remove();})
+}
 
+function createElm(b) {
+	let elm = document.createElement("DIV");
+	elm.classList.add("todo");
+
+	let span = document.createElement("SPAN");
+	span.classList.add("todo_text");
+	let t = document.createTextNode(b);
+	// Create a text node
+	span.appendChild(t);
+
+	let iconi = document.createElement("I");
+	iconi.classList.add("icon");
+	t = document.createTextNode("@");
+	// Create a text node
+	iconi.appendChild(t);
+
+	elm.appendChild(span);
+	elm.appendChild(iconi);
+
+	todo_wrap.appendChild(elm);
+}
 
 
 // add todo
@@ -67,32 +69,13 @@ var adder = document.getElementsByClassName("add-icon");
 adder[0].addEventListener("click", function(){
 	let b = input.value;
 
-	let elm = document.createElement("DIV");
-	elm.classList.add("todo");
-
-	let span = document.createElement("SPAN");
-	span.classList.add("todo_text");
-	var t = document.createTextNode(b);
-	// Create a text node
-    span.appendChild(t);
-
-	let iconi = document.createElement("I");
-	iconi.classList.add("icon");
-	t = document.createTextNode("@");
-	// Create a text node
-    iconi.appendChild(t);
-
-	elm.appendChild(span);
-	elm.appendChild(iconi);
-
-	todo_wrap.appendChild(elm);
+	createElm(b);
 
 	icon = document.getElementsByClassName("icon");
-	updateEvent();
+	updateEventIcon();
 
 	todo_text = document.getElementsByClassName("todo_text");
 	updateEventTodo();
 
 	input.value = "";
-	console.log(todo_text)
 });
